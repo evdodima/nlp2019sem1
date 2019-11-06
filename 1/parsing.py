329ddf93ec42aae08900012633f039cb5ada55fd
Dmitri Evdokimenko <evdodima@gmail.com>
2019-11-06 22:54:45 +0300

    parsing reworked

--- 1/parsing.py
+++ 1/parsing.py
@@ -2,8 +2,10 @@ import os
 import pubmed_parser as pp
 from pyspark.sql import Row
 
-path_to_data = pp.list_xml_path('parsed/')
-parsed_results = path_to_data.map(lambda x: Row(file_name=os.path.basename(x), **pp.parse_pubmed_xml(x)))
-dataframe = parsed_results.toDF()
+# path_to_data = pp.list_xml_path('parsed')
+# parsed_results = path_to_data.map(lambda x: Row(file_name=os.path.basename(x), **pp.parse_pubmed_xml(x)))
+parsed_results = pp.parse_medline_xml('../parsed/sample_data.xml')
+rows = parsed_results.map(lambda x: Row(**x))
+dataframe = rows.df()
 final_data = dataframe[['abstract']]
 final_data.write.parquet('abstracts.parquet')
