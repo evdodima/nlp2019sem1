4d6d0747c498d42244726069207bc4f6d160487f
Dmitri Evdokimenko <evdodima@gmail.com>
2019-11-06 22:28:52 +0300

    typo fix

--- 1/parsing.py
+++ 1/parsing.py
@@ -3,7 +3,7 @@ import pubmed_parser as pp
 from pyspark.sql import Row
 
 path_to_data = pp.list_xml_path('parsed/')
-parsed_results = pubmed_oa_rdd.map(lambda x: Row(file_name=os.path.basename(x), **pp.parse_pubmed_xml(x)))
+parsed_results = path_to_data.map(lambda x: Row(file_name=os.path.basename(x), **pp.parse_pubmed_xml(x)))
 dataframe = parsed_results.toDF()
 final_data = dataframe[['abstract']]
-pubmed_oa_df_sel.write.parquet('abstracts.parquet')
+final_data.write.parquet('abstracts.parquet')
