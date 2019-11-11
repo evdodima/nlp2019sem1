a50a7f3f50353e5c35afe4949494edb623a3277d
Rinat Khanov <rinat@khanov.com>
2019-11-11 13:10:09 +0300

    remove stop words

--- 2/processing.py
+++ 2/processing.py
@@ -4,3 +4,13 @@ from nltk.corpus import stopwords
 def word_tokenize1(x):
     lowerW = x.lower()
     return nltk.word_tokenize(x)
+
+# read data ...
+# data =
+
+words = data.flatMap(word_tokenize1)
+print words.collect()
+
+stop_words=set(stopwords.words('english'))
+stopW = words.filter(lambda word : word[0] not in stop_words and word[0] != '')
+#print stopW.collect()
