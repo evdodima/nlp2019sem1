c41e54c2a69c3510fb86809fcbb5cd131873b7b1
Rinat Khanov <rinat@khanov.com>
2019-11-16 14:57:36 +0300

    add new sample file

--- 2/processing.py
+++ 2/processing.py
@@ -1,16 +1,24 @@
-import nltk
-from nltk.corpus import stopwords
+# import nltk
+# from nltk.corpus import stopwords
 
-def word_tokenize1(x):
-    lowerW = x.lower()
-    return nltk.word_tokenize(x)
+# def word_tokenize1(x):
+#     lowerW = x.lower()
+#     return nltk.word_tokenize(x)
 
-# read data ...
-# data =
+# # read data ...
+# # data =
 
-words = data.flatMap(word_tokenize1)
-print words.collect()
+# words = data.flatMap(word_tokenize1)
+# print words.collect()
 
-stop_words=set(stopwords.words('english'))
-stopW = words.filter(lambda word : word[0] not in stop_words and word[0] != '')
-#print stopW.collect()
+# stop_words=set(stopwords.words('english'))
+# stopW = words.filter(lambda word : word[0] not in stop_words and word[0] != '')
+# #print stopW.collect()
+
+import time
+input = "16.11.2019 14:25" # Hook['params']['date']
+ts = time.strptime(input + " GMT+03:00", '%d.%m.%Y %H:%M GMT+03:00')
+timestamp = time.mktime(ts)
+print timestamp
+
+
