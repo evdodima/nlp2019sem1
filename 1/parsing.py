f4095b567633c3f3d8ffab9a69ba275a2e0ce5fb
Rinat Khanov <rinat@khanov.com>
2019-11-16 15:14:47 +0300

    fix

--- 1/parsing.py
+++ 1/parsing.py
@@ -4,8 +4,8 @@ from pyspark.sql import Row
 
 # path_to_data = pp.list_xml_path('parsed')
 # parsed_results = path_to_data.map(lambda x: Row(file_name=os.path.basename(x), **pp.parse_pubmed_xml(x)))
-parsed_results = pp.parse_medline_xml('../parsed/sample_data.xml')
-rows = parsed_results.map(lambda x: Row(**x))
+parsed_results = pp.parse_medline_xml('./parsed/new_sample.xml')
+rows = list(map(lambda x: Row(**x), parsed_results))
 dataframe = rows.df()
 final_data = dataframe[['abstract']]
 final_data.write.parquet('abstracts.parquet')
