134158a7b298c8307749d5a8b85872e4c2b598db
Rinat Khanov <rinat@khanov.com>
2019-11-18 11:36:31 +0300

    abstracts success parsing

--- 2/processing.py
+++ 2/processing.py
@@ -1,24 +1,46 @@
-# import nltk
-# from nltk.corpus import stopwords
+import nltk
+from nltk.corpus import stopwords
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
 
-# def word_tokenize1(x):
-#     lowerW = x.lower()
-#     return nltk.word_tokenize(x)
+import os
+from glob import glob
+import pubmed_parser as pp
+from pyspark.sql import Row
 
-# # read data ...
-# # data =
+conf = SparkConf().\
+    setAppName('map').\
+    setMaster('local[5]').\
+    set('spark.yarn.appMasterEnv.PYSPARK_PYTHON', '~/anaconda3/bin/python').\
+    set('spark.yarn.appMasterEnv.PYSPARK_DRIVER_PYTHON', '~/anaconda3/bin/python').\
+    set('executor.memory', '8g').\
+    set('spark.executor.memoryOverhead', '16g').\
+    set('spark.sql.codegen', 'true').\
+    set('spark.yarn.executor.memory', '16g').\
+    set('yarn.scheduler.minimum-allocation-mb', '500m').\
+    set('spark.dynamicAllocation.maxExecutors', '3').\
+    set('spark.driver.maxResultSize', '0')
 
-# words = data.flatMap(word_tokenize1)
-# print words.collect()
+spark = SparkSession.builder.\
+    appName("testing").\
+    config(conf=conf).\
+    getOrCreate()
 
-# stop_words=set(stopwords.words('english'))
-# stopW = words.filter(lambda word : word[0] not in stop_words and word[0] != '')
-# #print stopW.collect()
+spark.sparkContext.addPyFile('pubmed_parser/dist/pubmed_parser-0.2.1-py3.7.egg') # building with Python 3.5
 
-import time
-input = "16.11.2019 14:25" # Hook['params']['date']
-ts = time.strptime(input + " GMT+03:00", '%d.%m.%Y %H:%M GMT+03:00')
-timestamp = time.mktime(ts)
-print timestamp
+df = sqlContext.read.parquet('./raw_medline.parquet')
 
+def word_tokenize1(x):
+    lowerW = x.lower()
+    return nltk.word_tokenize(x)
+
+# read data ...
+# data =
+
+words = data.flatMap(word_tokenize1)
+print words.collect()
+
+stop_words=set(stopwords.words('english'))
+stopW = words.filter(lambda word : word[0] not in stop_words and word[0] != '')
+#print stopW.collect()
 
