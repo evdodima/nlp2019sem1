62d00645e220230897802fae2d854444ccca2d55
Dmitri Evdokimenko <evdodima@gmail.com>
2019-12-22 15:05:32 +0300

    6 edited

--- 6/6.py
+++ 6/6.py
@@ -1,19 +1,28 @@
 from sklearn import svm
 import numpy as np
 import pandas as pd
+import os
 
 from sklearn.metrics import classification_report
 
-vectors_train = pd.read_pickle('/Volumes/新加卷/nlp/vectors/out_train.pkl')
+input_path = "/Users/evdodima/education/pkls/"
 
-print(vectors_train)
+for filename in sorted(os.listdir(input_path)):
+	if filename.endswith("train.pkl"):
+		corpusname = filename.split("_")[0]
+		vectors_train = pd.read_pickle(input_path + filename)
+		# print(vectors_train)
 
-clf = svm.SVC()
-clf.fit(list(vectors_train['vector']), list(vectors_train['label']))
+		clf = svm.SVC(kernel='linear', 
+            class_weight='balanced', # penalize
+            probability=True)
+		clf.fit(list(vectors_train['vector']), list(vectors_train['label']))
 
 
-vectors_test = pd.read_pickle('/Volumes/新加卷/nlp/vectors/out_test.pkl')
+		vectors_test = pd.read_pickle(input_path + corpusname + "_test.pkl")
 
-predictions = clf.predict(list(vectors_test['vector']))
+		y_pred = clf.predict(list(vectors_test['vector']))
+		y_test = list(vectors_test['label'])
 
-print(classification_report(list(vectors_test['label']), predictions))
+		print(corpusname)
+		print(classification_report(y_true=y_test,y_pred=y_pred))
