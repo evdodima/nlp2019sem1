dfa76bee5306a6962b42bca27fc08120f16ca87f
Dmitri Evdokimenko <evdodima@gmail.com>
2019-12-26 11:47:50 +0300

    code cleanup

--- 3/learning_script/learning_script.py
+++ 3/learning_script/learning_script.py
@@ -16,56 +16,42 @@ args = parser.parse_args()
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
-
-modelTypes = ['skipgram', 'cbow']
-dims = [300,600]
-windows = [5,10]
-
-base = "/Volumes/新加卷/nlp/"
-
-vocab_dima = {}
+vocab = {}
 
 in_dir = args.in_dir
 dict_path = args.dict_path
 out_dir = args.out_dir
 
 with open(dict_path,"rb") as lines:
-	vocab_dima = dict(pickle.load(lines))
-# print(vocab_dima)
+	vocab = dict(pickle.load(lines))
 
-for path_dima in sorted(os.listdir(in_dir)):
-	if path_dima.endswith(".txt") and path_dima != 'all.txt':
-		full_path_dima = in_dir + "/" + path_dima
-		with open(full_path_dima, "r") as file_dima:
-			tatal_ex = 0
-			tatal_words = 0
-			for sentence in file_dima:
+
+for path in sorted(os.listdir(in_dir)):
+	if path.endswith(".txt") and path != 'all.txt':
+		full_path = in_dir + "/" + path + "/part-00000"
+		with open(full_path, "r") as input_file:
+			total_ex = 0
+			total_words = 0
+			for sentence in input_file:
 				for word in sentence.split(" "):
-					tatal_words += 1
-				tatal_ex += 1
-			print(path_dima,tatal_ex,tatal_words)
+					total_words += 1
+				total_ex += 1
+			print(path,total_ex,total_words)
 
-			for mt in modelTypes:
-				for dim in dims:
-					for window in windows:
-						name = str((mt,dim,window))
+			model_name = "fasttext_cbow_300_w10.model"
+			model_path = out_dir + "/" + model_name
+			model = None
 
-						print("started " + name + " on " + path_dima)
+			if os.path.exists(model_path):
+				model = FastText.load(model_path)
+			else:
+				model = FastText(sg=0,size=300, window=10, min_n=3,max_n=5, min_count=1)  # instantiate
 
-						fname = out_dir + "/" + name + '.model'
+				model.build_vocab_from_freq(vocab)
+				print("finished vocab")
 
-						model = None
-						if os.path.exists(fname):
-							model = FastText.load(fname)
-						else:
-							model = FastText(sg=1 if mt == "skipgram" else 0,size=dim, window=window, min_n=3,max_n=5, min_count=1)  # instantiate
+			model.train(corpus_file = full_path, total_examples=total_ex, total_words=total_words, epochs=model.epochs)  # train
 
-							model.build_vocab_from_freq(vocab_dima)
-							print("finished vocab")
-
-
-						model.train(corpus_file = full_path_dima, total_examples=tatal_ex, total_words=tatal_words, epochs=model.epochs)  # train
-
-						model.save(get_tmpfile(fname))
+			model.save(get_tmpfile(model_path))
 
 
--- 5/load_vecs.py
+++
@@ -1,3 +0,0 @@
-from gensim.models import KeyedVectors
-wv_from_text = KeyedVectors.load_word2vec_format('/Users/evdodima/education/fasttext_pmc.vec', binary=False)
-print(wv_from_text.get_vector("man"))
